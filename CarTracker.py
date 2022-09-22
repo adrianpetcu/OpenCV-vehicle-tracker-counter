5bcc84923e672edb458a235ace969a4eccfaabb9
Adrian Petcu <3684438+adrianpetcu@users.noreply.github.com>
2022-09-22 22:48:58 +0300

    Update CarTracker.py
    modified to match middle lane highway count from the local rtsp stream and publish event

--- CarTracker.py
+++ CarTracker.py
@@ -1,9 +1,13 @@
 import cv2
+import datetime
+import paho.mqtt.client as mqtt
 from tracker import *
 
 tracker = EuclideanDistTracker()
 
-cap = cv2.VideoCapture("testfootage.mp4")
+cap = cv2.VideoCapture("rtsp://172.28.95.99:8554/stream") #match the local highway stream
+mqtt_client = mqtt.Client("detector")
+mqtt_client.connect("127.0.0.1") #connect to local mqtt
 
 object_detector = cv2.createBackgroundSubtractorMOG2(history=100,varThreshold = 30)
 count = 0
@@ -12,13 +16,13 @@ counted_id=[]
 while True:
     ret, frame = cap.read()
     
-    y=0
-    x=464
-    h=185
-    w=464
+    y=455
+    x=950
+    h=950
+    w=950
     
     if ret == True:
-        roi = frame[y:y+h, x:x+w].copy()
+        roi = frame[y:y+h, x:x+w].copy() #take the middle lane of the road
         
         mask = object_detector.apply(roi)
         _,mask = cv2.threshold(mask,254,255, cv2.THRESH_BINARY)
@@ -31,7 +35,7 @@ while True:
                 cv2.rectangle(roi,(x,y),(x+w,y+h), (0,255,0),3)
                 detections.append([x,y,w,h])
                 
-        cv2.line(roi,(232,0),(232,185),(0,0,255),1)
+        cv2.line(roi,(0,350),(950,350),(0,0,255),1)
         boxes_ids = tracker.update(detections)
         for box_id in boxes_ids:
             x,y,w,h,ids= box_id
@@ -40,12 +44,13 @@ while True:
             cv2.circle(roi,(centerx,centery),2,(0,0,255),5)
             if centerx > 212 and centerx < 252:
                 if ids not in counted_id:
+                    mqtt_client.publish(str(datetime.datetime.now()) + ":new_car")    #publish basic new car event
                     count2 +=1
                     counted_id.append(ids)
-            cv2.line(roi,(232,0),(232,185),(0,255,0),2)
+            cv2.line(roi,(0,350),(950,350),(0,0,255),2)
             count = ids
             
-        #cv2.putText(roi,str(count),(10,25), cv2.FONT_HERSHEY_PLAIN,2,(255,0,0),2)
+        
         cv2.putText(roi,str(count2),(10,25), cv2.FONT_HERSHEY_PLAIN,2,(255,0,0),2)
         cv2.imshow("ROI", roi)
         cv2.imshow("Feed", frame)
